8ede893b9c89c491d159e242331533e3e60181a7
Tsuchikawa Toshiki <toshiki@Tsuchikawa-no-MacBook-Air.local>
2016-08-27 20:30:12 +0900

    admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :pictures, only: [:index, :new, :create, :edit, :update, :destroy]
   root 'pictures#index'
   # The priority is based upon order of creation: first created -> highest priority.
