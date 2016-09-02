c879fd7f0e1ea4e05188d0921ec05505ba8abe25
Tsuchikawa Toshiki <toshiki@Tsuchikawa-no-MacBook-Air.local>
2016-09-02 14:32:11 +0900

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160901083656) do
+ActiveRecord::Schema.define(version: 20160902044437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20160901083656) do
     t.string   "image"
     t.integer  "user_id"
     t.text     "content"
+    t.integer  "topix_id"
   end
 
   create_table "users", force: :cascade do |t|
